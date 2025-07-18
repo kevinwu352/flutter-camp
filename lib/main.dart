756f77db68fa717c31a224a90844be37194efb0f
kevin <kevinwu352@gmail.com>
2025-07-18 15:58:18 +0800

    002

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Camp',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.brown, brightness: Brightness.light),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.brown,
+          brightness: Brightness.light,
+        ),
       ),
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: Text("Flutter Camp"),
         ),
-        body: Text("hello"),
+        body: Text("abc"),
+        // body: Image.asset(
+        //   'assets/images/bg.jpg',
+        //   height: double.infinity,
+        //   fit: BoxFit.cover,
+        // ),
+        // body: Image.network(
+        //   "https://cdn.pixabay.com/photo/2012/11/28/09/32/explosion-67557_1280.jpg",
+        //   height: double.infinity,
+        //   fit: BoxFit.cover,
+        // ),
       ),
     );
   }
