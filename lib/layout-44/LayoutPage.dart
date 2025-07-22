8fa2914ea4ff1a40d2ce37bd15d82b3b2de9f467
kevin <kevinwu352@gmail.com>
2025-07-22 09:30:35 +0800

    016

--- lib/layout-44/LayoutPage.dart
+++ lib/layout-44/LayoutPage.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class LayoutPage extends StatelessWidget {
@@ -50,6 +52,7 @@ class LayoutPage extends StatelessWidget {
     // );
 
     // Container
+    // 简单总结：有子随子，有对准时将约束放松；无子随父，父无限时尺寸是 0
     // Widget? child
     // Color? color 背景色，如果要渐变或图片用 decoration，用 decoration 这里必须置空
     // double? width
@@ -69,44 +72,60 @@ class LayoutPage extends StatelessWidget {
 
     // Clip clipBehavior = Clip.none
 
-    // to honor alignment
-    // to size itself to the child
-    // to honor the width, height, and constraints
-    // to expand to fit the parent
-    // to be as small as possible
-
-    // If the widget has no child, no height, no width, no constraints, and the parent provides unbounded constraints, then Container tries to size as small as possible.
-    // If the widget has no child and no alignment, but a height, width, or constraints are provided, then the Container tries to be as small as possible given the combination of those constraints and the parent's constraints.
-    // If the widget has no child, no height, no width, no constraints, and no alignment, but the parent provides bounded constraints, then Container expands to fit the constraints provided by the parent.
-    // If the widget has an alignment, and the parent provides unbounded constraints, then the Container tries to size itself around the child.
-    // If the widget has an alignment, and the parent provides bounded constraints, then the Container tries to expand to fit the parent, and then positions the child within itself as per the alignment.
-
-    // a) 无子 无宽高约束 父给无限约束，尽量小
+    // a) 无子 无对准 无宽高约束，父给有限约束则尽量大，父给无限约束则尽量小
     // return Center(child: Container(color: Colors.red)); // Center 给子有限约束
     // return UnconstrainedBox(child: Container(color: Colors.red)); // UnconstrainedBox 给子无限约束
     // b) 无子 无对准 有宽高约束，和父给的约束一起决定，尽量小
-    return Center(child: Container(color: Colors.red, width: 100, height: 100));
+    // return Center(child: Container(color: Colors.red, width: 100, height: 100));
     // return Center(
     //   child: Container(
     //     color: Colors.red,
     //     // constraints: BoxConstraints.tight(Size(50, 50)),
     //     // constraints: BoxConstraints.expand(width: 50, height: 50), // 有值是紧约束，该是 50
     //     // constraints: BoxConstraints.expand(), // 无值是紧无限，真的撑满屏了
-    //     constraints: BoxConstraints.loose(Size(50, 50)), // 0-50 松，不应该是 0 吗？
-    //     // constraints: BoxConstraints.tightFor(), // 0-inf 松，不应该是 0 吗？
+    //     // constraints: BoxConstraints.loose(Size(50, 50)), // 0-50 松，尺寸 50，不应该是 0 吗？
+    //     // constraints: BoxConstraints.tightFor(), // 0-inf 松，尺寸撑满，不应该是 0 吗？
     //   ),
     // );
-    // c) 无子 无宽高约束 无对准 父给约束，适配父的约束
-    // d) 有对准 父给无限约束，和子一样大
-    // e) 有对准 父给有限约束，扩大适配父，子找位置对准
     // return Center(
     //   child: Container(
-    //     alignment: Alignment.topRight,
-    //     width: 300,
-    //     color: Colors.green,
+    //     color: Colors.red,
+    //     width: 200,
+    //     height: 200,
+    //     // constraints: BoxConstraints.tight(Size(50, 50)), // 优先级高于宽高
+    //     // constraints: BoxConstraints.expand(), // 优先级高于宽高
+    //     // constraints: BoxConstraints.loose(Size(50, 50)), // 优先级高于宽高，尺寸 50
+    //     // constraints: BoxConstraints.tightFor(), // 0-inf 松，尺寸 200
+    //   ),
+    // );
+    // c) 有子 有对准 父给有限约束，扩大适配父，子找位置对准
+    // return Center(
+    //   child: Container(
+    //     alignment: Alignment.centerRight,
+    //     color: Colors.red,
     //     child: Text("abc"),
     //   ),
     // );
+    // d) 有子 有对准 父给无限约束，缩小适配子
+    // return UnconstrainedBox(
+    //   child: Container(
+    //     alignment: Alignment.centerRight,
+    //     color: Colors.red,
+    //     child: Text("abc"),
+    //   ),
+    // );
+    // 其它修饰
+    return Center(
+      child: Container(
+        decoration: BoxDecoration(
+          shape: BoxShape.rectangle,
+          color: Colors.green,
+        ),
+        // padding: EdgeInsets.symmetric(vertical: 50),
+        // transform: Matrix4.rotationZ(pi / 4),
+        child: Text("abc"),
+      ),
+    );
 
     // return Text("data");
   }
