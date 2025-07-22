166fc88b5bfba6955238fd4961c7930175183d55
kevin <kevinwu352@gmail.com>
2025-07-22 19:19:05 +0800

    023

--- lib/layout-44/box_page.dart
+++ lib/layout-44/box_page.dart
@@ -1,16 +1,9 @@
 import 'package:flutter/material.dart';
 
-// ConstrainedBox
-// UnconstrainedBox
-
 // FittedBox 缩放内容，只缩放有限，否则会崩
 // LimitedBox 只约束无限，这应该叫有限盒子
 
 // OverflowBox 溢出不警告，手动设置要传给子的约束
-// SizedBox 固定尺寸，如果传无限则占满可用空间expand，无子时用来撑开间距
-
-// SizedOverflowBox
-// FractionallySizedBox
 
 class BoxPage extends StatelessWidget {
   const BoxPage({super.key});
@@ -38,6 +31,7 @@ class BoxPage extends StatelessWidget {
     //   ),
     // );
 
+    // SizedBox 固定尺寸，如果传无限则占满可用空间expand，无子时用来撑开间距
     // return Center(
     //   // 固定尺寸
     //   // child: SizedBox( // 往下传紧约束 300*60
@@ -52,6 +46,53 @@ class BoxPage extends StatelessWidget {
     //   // child: SizedBox.shrink( // 收到的是松约束，向下传紧的 0*0
     //   //   child: ElevatedButton(onPressed: () {}, child: Text("doit")),
     //   // ),
+    // );
+
+    // SizedOverflowBox 同 SizedBox，但向下传的约束是原始的自己收到的。可能会溢出，但没溢出警告
+    // 盒子是 200*200，超出的内容也不切掉
+    // 向下传松约束 0-402 0-874
+    // return Center(
+    //   child: SizedOverflowBox(
+    //     size: Size(200, 200),
+    //     alignment: Alignment.centerRight,
+    //     child: Text("god is a girl"),
+    //   ),
+    // );
+    // 这个例子抄的，更直观
+    // return Center(
+    //   child: Container(
+    //     color: Colors.orange,
+    //     child: SizedOverflowBox(
+    //       size: Size(200, 200),
+    //       alignment: Alignment.centerRight,
+    //       child: Text("god is a girl"),
+    //     ),
+    //   ),
+    // );
+
+    // FractionallySizedBox 用父的百分比宽高
+    // 向下传紧约束 201 437
+    // 无子时也当成百分比间隔来用，像 SizedBox 一样
+    // return Center(
+    //   child: FractionallySizedBox(
+    //     widthFactor: 0.5,
+    //     heightFactor: 0.5,
+    //     child: ElevatedButton(onPressed: () {}, child: Text("doit")),
+    //     // child: DecoratedBox(
+    //     //   decoration: BoxDecoration(
+    //     //     border: Border.all(color: Colors.blue, width: 1),
+    //     //   ),
+    //     // ),
+    //   ),
+    // );
+    // 直接用在 Column/Row 里面会崩
+    // 但用 Expanded 包起来也不是我想要的，我想要的是0.2屏幕高的空白，结果把文字顶到头尾了，咋办？
+    // return Column(
+    //   children: [
+    //     Text("aaaaa"),
+    //     Expanded(child: FractionallySizedBox(heightFactor: 0.2)),
+    //     Text("bbbbb"),
+    //   ],
     // );
 
     return Text("data");
