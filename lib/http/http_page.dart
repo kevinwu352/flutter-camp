23f5bdba7468e61231ad3b9d451dea271c42ccaf
kevin <kevinwu352@gmail.com>
2025-07-28 18:55:38 +0800

    053

---
+++ lib/http/http_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class User {
+  final String name;
+  final int age;
+  @override
+  String toString() {
+    return "$name $age";
+  }
+
+  const User({required this.name, required this.age});
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return switch (json) {
+      {'name': String name, 'age': int age} => User(name: name, age: age),
+      _ => throw FormatException('failed to load customer'),
+    };
+  }
+}
+
+class HttpPage extends StatelessWidget {
+  const HttpPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Home")),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          print("begin");
+          final response = await http.get(
+            Uri.parse('https://dummyjson.com/c/660c-9047-47c3-b9db'),
+          );
+          if (response.statusCode == 200) {
+            // If the server did return a 200 OK response,
+            // then parse the JSON.
+            final c = User.fromJson(
+              jsonDecode(response.body) as Map<String, dynamic>,
+            );
+            print(c);
+          }
+
+          // var url = 'https://pae.ipportalegre.pt/testes2/wsjson/api/app/ws-authenticate';
+          // var url = 'https://dummyjson.com/c/660c-9047-47c3-b9db';
+
+          // Map data = {'apikey': '12345678901234567890'};
+
+          // var body = json.encode(data);
+
+          // var response = await http.post(
+          //   Uri.parse(url),
+          //   headers: {"Content-Type": "application/json"},
+          //   body: body,
+          // );
+          // print(response.body);
+        },
+        child: Icon(Icons.play_circle),
+      ),
+      body: Text("body"),
+    );
+  }
+}
---
+++ lib/http/json_page.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'dart:convert';
+
+// list => model
+//   reviewsData.map((reviewData) => Review.fromJson(reviewData)).toList()
+// model => list
+//   reviews.map((review) => review.toJson()).toList()
+
+class User {
+  final String name;
+  final int age;
+  @override
+  String toString() {
+    return "$name $age";
+  }
+
+  const User({required this.name, required this.age});
+
+  // 1) 类型不同时，会异常，就算 age 是可选，模式匹配也用可选，也会异常
+  // 2) 缺少字段时，会异常，可选也不行
+  factory User.fromJson1(Map<String, dynamic> json) {
+    return switch (json) {
+      {'name': String name, 'age': int age} => User(name: name, age: age),
+      _ => throw FormatException('failed to load customer'),
+    };
+  }
+  // 1) 类型不同时，as 直接异常，as int? 也不行
+  // 2) 缺少字段时， as 直接异常，as int? 能成功
+  factory User.fromJson2(Map<String, dynamic> data) {
+    final name = data['name'] as String;
+    final age = data['age'] as int;
+    return User(name: name, age: age);
+  }
+}
+
+class JsonPage extends StatelessWidget {
+  const JsonPage({super.key});
+
+  void parse() {
+    try {
+      // final str = '{"name":"kevin","age":18}';
+      final str = '{"name":"kevin","age":"19"}';
+      final json = jsonDecode(str) as Map<String, dynamic>;
+      print(json);
+      final cs = User.fromJson1(json);
+      print(cs);
+    } catch (e) {
+      print("got $e");
+    } finally {
+      print("clean up");
+    }
+  }
+
+  User work(String str) {
+    print("111");
+    // final str = '{"name":"kevin","age":"19"}';
+    final json = jsonDecode(str) as Map<String, dynamic>;
+    print(json);
+    final cs = User.fromJson1(json);
+    print(cs);
+    print("222");
+    return cs;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Home")),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          // compute 里面的异常也能在外面被抓住
+          try {
+            print("begin");
+            final str = '{"name":"kevin","age":"19"}';
+            final cs = await compute(work, str);
+            print(cs);
+          } catch (e) {
+            print("got $e");
+          } finally {
+            print("clean up");
+          }
+        },
+        child: Icon(Icons.play_circle),
+      ),
+      body: Text("body"),
+    );
+  }
+}
