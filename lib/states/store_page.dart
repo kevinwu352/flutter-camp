404ab70560843aa79c2dcae850b520d616a49016
kevin <kevinwu352@gmail.com>
2025-07-29 10:14:06 +0800

    056

--- lib/states/store_page.dart
+++ lib/states/store_page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+
+// 读写普通键值对，不要用这
 // import 'package:shared_preferences/shared_preferences.dart';
 // final pref = await SharedPreferences.getInstance();
 // final key = "app_boarded";
@@ -8,11 +13,25 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 // final val = await pref.getBool(key);
 // await pref.remove(key);
 
+// 读写安全键值对
 // final storage = FlutterSecureStorage();
+//
 // await storage.write(key: "acc_key", value: "11112222");
 // await storage.write(key: "acc_key", value: null);
+//
 // var val = await storage.read(key: "acc_key");
+//
 // await storage.delete(key: "acc_key");
+
+// 读写文件
+// final dir = await getApplicationDocumentsDirectory();
+// final file = File('${dir.path}/counter.txt');
+//
+// await file.writeAsString('101');
+//
+// final contents = await file.readAsString();
+// final val = int.parse(contents);
+// print(val);
 
 class StorePage extends StatelessWidget {
   const StorePage({super.key});
@@ -24,15 +43,16 @@ class StorePage extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           print("begin");
-          final storage = FlutterSecureStorage();
-          await storage.write(key: "acc_key", value: "11112222");
-          // await storage.write(key: "acc_key", value: null);
-          print("done");
-          var val = await storage.read(key: "acc_key");
+
+          final dir = await getApplicationDocumentsDirectory();
+          final file = File('${dir.path}/counter.txt');
+
+          // await file.writeAsString('101');
+
+          final contents = await file.readAsString();
+          final val = int.parse(contents);
           print(val);
-          await storage.delete(key: "acc_key");
-          val = await storage.read(key: "acc_key");
-          print(val);
+
           print("end");
         },
         child: Icon(Icons.run_circle_outlined),
