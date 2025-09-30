31a1d8509d5960c0262a64e78ef4073659099a72
kevin <kevinwu352@gmail.com>
2025-09-30 12:12:44 +0800

    186

--- lib/25scroll/page_page.dart
+++ lib/25scroll/page_page.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import '/core/core.dart';
 
+// PageView 没有缓存功能，滑出屏幕的页面会销毁
+// allowImplicitScrolling 能帮助缓存前后一页，但更前更后的就不行了
+// 最佳缓存方案要用 AutomaticKeepAliveClientMixin，见下例
+
+// ================================================================================
+
 // .builder
 // int? itemCount
 // required NullableIndexedWidgetBuilder itemBuilder
@@ -12,8 +18,6 @@ import '/core/core.dart';
 
 // PageController.viewportFraction 单个页面的宽度占据视窗多少比例
 
-// ==========
-
 class PagePage extends StatefulWidget {
   const PagePage({super.key});
 
@@ -22,7 +26,7 @@ class PagePage extends StatefulWidget {
 }
 
 class _PagePageState extends State<PagePage> {
-  final pc = PageController(viewportFraction: 0.6);
+  final pc = PageController();
 
   @override
   Widget build(BuildContext context) {
@@ -30,21 +34,49 @@ class _PagePageState extends State<PagePage> {
       appBar: AppBar(title: Text('Page')),
       floatingActionButton: FloatingActionButton(onPressed: () {}, child: Icon(Icons.run_circle)),
       body: PageView.builder(
-        controller: pc,
-        pageSnapping: false,
-        padEnds: true,
-        itemCount: 3,
-        itemBuilder: (context, index) => Container(color: ColorsExt.rand()),
+        // controller: pc,
+        // pageSnapping: false,
+        // padEnds: true,
+        itemCount: 5,
+        // itemBuilder: (context, index) => Container(color: ColorsExt.rand()),
+        itemBuilder: (context, index) => SubPage(name: '$index'),
+        allowImplicitScrolling: true,
       ),
     );
   }
 }
 
 class SubPage extends StatelessWidget {
-  const SubPage({super.key});
-
+  SubPage({super.key, required this.name}) {
+    print('init $name');
+  }
+  final String name;
   @override
   Widget build(BuildContext context) {
     return Container(color: ColorsExt.rand());
   }
+}
+
+// 让子页面能够缓存，分成三步
+class ChildPage extends StatefulWidget {
+  ChildPage({super.key, required this.name}) {
+    print('init $name');
+  }
+  final String name;
+
+  @override
+  State<ChildPage> createState() => _ChildPageState();
+}
+
+// 1) 增加 mixin
+class _ChildPageState extends State<ChildPage> with AutomaticKeepAliveClientMixin {
+  @override
+  Widget build(BuildContext context) {
+    super.build(context); // 2) 调用 super.build
+    return Container(color: ColorsExt.rand());
+  }
+
+  // 3) 添加属性
+  @override
+  bool get wantKeepAlive => true;
 }
