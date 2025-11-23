39a6818f5fb936e044ca0e31d6aad91e85d77b7e
kevin <kevinwu352@gmail.com>
2025-11-23 09:57:29 +0800

    245

--- lib/12states/hive_ext.dart
+++ lib/12states/hive_ext.dart
@@ -2,7 +2,7 @@ import 'package:hive/hive.dart';
 
 import '/core/core.dart';
 
-extension HiveBoxExt<E> on Box<E> {
+extension HiveBoxE<E> on Box<E> {
   Future<void> setValue(String? key, E? value) async {
     if (key is String) {
       if (value is E) {
@@ -22,25 +22,23 @@ extension HiveBoxExt<E> on Box<E> {
   double? getDouble(String? key) => getValue(key).asOr<double>();
   String? getString(String? key) => getValue(key).asOr<String>();
 
+  Map<String, Object?>? getMap(String? key) => getValue(key).asOr<Map>()?.map((k, v) => MapEntry(k.toString(), v));
+
   List<Object?>? getList(String? key) => getValue(key).asOr<List>();
+  List<T>? getListOf<T>(String? key) => getValue(key).asOr<List>()?.whereType<T>().toList();
 
   // ================================================================================
 
-  Map<String, Object?>? getMap(String? key) => getValue(key).asOr<Map>()?.map((k, v) => MapEntry(k.toString(), v));
+  T? getObject<T>(String? key, T Function(Map<String, dynamic>) init) =>
+      withValue(getMap(key), (v) => v != null ? init(v) : null);
+
+  // ================================================================================
 
   List<Map<String, Object?>?>? getMapList(String? key) => getList(key)
       ?.where((e) => e == null || e is Map)
       .map((e) => e is Map ? e.map((k, v) => MapEntry(k.toString(), v)) : null)
       .toList();
 
-  // ================================================================================
-
-  T? getObject<T>(String? key, T Function(Map<String, dynamic>) init) {
-    final map = getMap(key);
-    return map != null ? init(map) : null;
-  }
-
-  List<T?>? getObjectList<T>(String? key, T Function(Map<String, dynamic>) init) {
-    return getMapList(key)?.map((e) => e != null ? init(e) : null).toList();
-  }
+  List<T?>? getObjectList<T>(String? key, T Function(Map<String, dynamic>) init) =>
+      getMapList(key)?.map((e) => e != null ? init(e) : null).toList();
 }
